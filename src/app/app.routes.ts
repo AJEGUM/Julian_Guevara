fb56b96927796dcd3605f9336845185e5efaadbd
AJEGUM <juliandguevara12@gmail.com>
2026-01-21 22:59:10 -0500

    Rutas comenario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-
+    // Rutas de la landing pages
     { path: 'Inicio', loadComponent: () => import('./pages/inicio/inicio').then(m => m.Inicio) },
     { path: '', redirectTo: 'Inicio', pathMatch: 'full' },
     { path: '**', redirectTo: 'Inicio' }
